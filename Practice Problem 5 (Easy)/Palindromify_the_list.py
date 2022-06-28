4e16bfa8810d10f170d2ce927212c42dfd8a7fbd
Harshkumar5898 <107399707+Harshkumar5898@users.noreply.github.com>
2022-06-28 14:52:50 +0530

    Add files via upload

---
+++ Practice Problem 5 (Easy)/Palindromify_the_list.py
@@ -0,0 +1,33 @@
+try:
+    no = int(input(" - Enter the total number of test - \n"))
+    list = []
+    for i in range(no):
+        a = int(input("Enter a number of which you want to find next palindrome : "))
+        list.append(a)
+
+except Exception as e:
+    print(f"There is an error [{e}]")
+else:
+    def reverse(num):
+        rev = 0
+        while num:
+            dig = num % 10
+            rev = rev * 10 + dig
+            num = num//10
+        return rev
+
+    for i in list:
+        if i > 10:
+            temp = i
+            if i == reverse(i):
+                print(f"your number {i} is already a palindeome")
+
+            else:
+                while True:
+                    temp += 1
+                    if temp == reverse(temp):
+                        print(f"Next palindrome for {i} is {temp}")
+                        break
+
+        else:
+            print(f"Your number {i} is smaller than 10")
